4260735c1352d39ee79350e5d81124297f005f12
AkshayKPrajapati <akshayprajapati6200@outlook.com>
2025-08-16 15:37:19 +0530

    create : start java coding

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public Main(){
+        System.out.println("This is a default constructore !!!");
+    }
+    @SuppressWarnings("unused")
+    public static void main (String []a){
+        Main main=new Main();
+    }
+}
