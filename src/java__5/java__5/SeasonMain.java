3b0517c93f79be4308dfc116672e667139c08fee
AkshayKPrajapati <akshayprajapati6200@outlook.com>
2025-08-20 00:18:48 +0530

    Enhance for loop

--- src/java__5/java__5/SeasonMain.java
+++ src/java__5/java__5/SeasonMain.java
@@ -2,22 +2,24 @@ import java.util.Scanner;
 
 public class SeasonMain {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the Season: ");
-        String input = scanner.next();
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Enter the Season: ");
+            String input = scanner.next();
 
-        Season[] seasons = Season.values();
-        boolean status=false;
-        for(int i=0;i<seasons.length;i++){
-            Season season=seasons[i];
-            if(input.equalsIgnoreCase(season.toString())){
-                System.out.println("valid season");
-                status=false;
-                break;
+            Season[] seasons = Season.values();
+            boolean status=false;
+            for(int i=0;i<seasons.length;i++){
+                Season season=seasons[i];
+                if(input.equalsIgnoreCase(season.toString())){
+                    System.out.println("valid season");
+                    status=false;
+                    break;
+                }
+            }
+            if(status==false){
+                System.out.println("Invalid season of the : "+input.toString());
             }
         }
-        if(status==false){
-            System.out.println("Invalid season of the : "+input.toString());
-        }
+        Season.WINTER.display();
     }
 }
